cb8dba504c3f8a1e196489c69453fc62af6cf233
Samuel Bryner <samuelbryner@gmx.ch>
2018-03-05 00:55:20 +0100

    Basic networking

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import selectors
 import sys
 
 from crossword import Crossword
+from management_interface import ManagementInterface
 
 log = logging.getLogger('puzzle')
 hdlr = logging.FileHandler('puzzle.log')
@@ -16,36 +17,6 @@ log.addHandler(hdlr)
 log.setLevel(logging.DEBUG)
 
 
-"""
-class CrosswordForm(npyscreen.Form):
-    def afterEditing(self):
-        self.parentApp.setNextForm(None)
-
-    def create(self):
-
-        #self.myName        = self.add(npyscreen.TitleText, name='Name')
-        #self.myDepartment = self.add(npyscreen.TitleSelectOne, scroll_exit=True, max_height=3, name='Department', values = ['Department 1', 'Department 2', 'Department 3'])
-        #self.myDate        = self.add(npyscreen.TitleDateCombo, name='Date Employed')
-        with open('puzzle.cfg', 'r') as puzzle_cfg:
-            self.crossword = self.add(Crossword, cfg=json.load(puzzle_cfg))
-
-        self.keypress_timeout = 0
-        self.curses_pad.nodelay(True) # non-blocking getch()
-
-    def while_waiting(self):
-        log.info("waiting in form...")
-
-class MyApplication(npyscreen.NPSAppManaged):
-    def onStart(self):
-        npyscreen.setTheme(npyscreen.Themes.ElegantTheme)
-        self.addForm('MAIN', CrosswordForm, name='Stadtwache Ankh Morpork Management Console Login')
-        # A real application might define more forms here.......
-
-
-        log.info("curses has color? {}".format(curses.has_colors()))
-        log.info("colors disabled? {}".format(npyscreen.npysGlobalOptions.DISABLE_ALL_COLORS))
-
-"""
 
 sel = selectors.DefaultSelector()
 
@@ -66,7 +37,7 @@ def main(screen):
 
     screen.refresh()
 
-    def read_stdin(stdin, mask, screen):
+    def read_stdin(stdin, screen):
         k = screen.getch()
         if k >= 0:
             if puzzle.handle_input(k):
@@ -76,12 +47,15 @@ def main(screen):
     # register key handler
     sel.register(sys.stdin, selectors.EVENT_READ, read_stdin)
 
-    while True:
-        events = sel.select()
-        for key, mask in events:
-            callback = key.data
-            callback(key.fileobj, mask, screen)
-            screen.refresh()
+    # create server for remote control
+    with ManagementInterface(1234, sel) as mi:
+
+        while True:
+            events = sel.select()
+            for key, mask in events:
+                callback = key.data
+                callback(key.fileobj, screen)
+                screen.refresh()
 
 if __name__ == '__main__':
 
---
+++ management_interface.py
@@ -0,0 +1,107 @@
+import socket
+import logging
+import selectors
+
+log = logging.getLogger('puzzle')
+
+class PacketParser:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.length = None
+        self.buffer = bytearray()
+
+    def parse(self, data):
+        self.buffer.extend(data)
+        if self.length is None:
+            # no length information yet
+            while b'\n' in self.buffer:
+                # got complete length info
+                payload_length, self.buffer = self.buffer.split(b'\n', 1)
+
+                # strip any additional newline characters
+                payload_length = payload_length.replace(b'\r', b'')
+                payload_length = payload_length.replace(b'\n', b'')
+
+                if len(payload_length) == 0:
+                    continue # try again by splitting at next newline
+
+                self.length = int(payload_length)
+
+                #log.info("got packet header: length = {}".format(int(payload_length)))
+                break
+
+        # no elseif, might fall trough:
+        if self.length is not None:
+            if len(self.buffer) >= self.length:
+                # packet is complete
+                packet = self.buffer[:self.length].decode('ascii')
+
+                # get ready for next packet
+                self.buffer = self.buffer[self.length:] # put rest of data (if any) in buffer for next packet
+                self.length = None
+
+                return packet
+
+
+
+class ManagementInterface:
+    def __init__(self, port, selector):
+        self.selector = selector
+
+
+        self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # could set SO_REUSEADDR so we can restart a program immediately
+        # but this could theoretically lead to some errorenous behaviour in edge cases
+        self.server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        self.server_sock.bind(('', port)) # bind to all available interfaces
+        self.server_sock.listen()
+        self.server_sock.setblocking(False)
+        self.selector.register(self.server_sock, selectors.EVENT_READ, self.accept)
+        log.info('started server on TODO:my ip')
+        # TODO: write it to screen!
+
+        self.data_buffer = {}
+        self.connections = []
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        log.info('closing server connection')
+        self.server_sock.shutdown(socket.SHUT_RDWR)
+        self.server_sock.close()
+        for conn in self.connections:
+            log.info('closing connection {}'.format(conn))
+            conn.close()
+
+
+    def accept(self, sock, screen):
+        conn, addr = sock.accept()  # Should be ready
+        log.info("accepted connection from {}\n".format(addr))
+        conn.setblocking(False)
+        self.selector.register(conn, selectors.EVENT_READ, self.read)
+
+        self.data_buffer[conn] = PacketParser()
+        self.connections.append(conn)
+
+    def read(self, conn, screen):
+        data = conn.recv(4096)
+
+        if data:
+            #log.info("received {} bytes: '{}'".format(len(data), data))
+            p = self.data_buffer[conn].parse(data)
+            if p:
+                self.handle_packet(p)
+        else:
+            log.info("closing connection {}\n".format(conn))
+            self.selector.unregister(conn)
+            conn.close()
+            self.connections.remove(conn)
+
+    def handle_packet(self, packet):
+        log.info("got packet: '{}'".format(repr(packet)))
+        pass
