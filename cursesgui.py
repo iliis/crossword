46692e2f6a7ba97ddb6d42a5a9bbfa3e59358ddb
Samuel Bryner <samuelbryner@gmx.ch>
2018-03-04 18:48:59 +0100

    Basic functionality works :)

--- cursesgui.py
+++ cursesgui.py
@@ -3,7 +3,15 @@
 import npyscreen
 import curses
 import json
+import logging
 from enum import Enum
+
+log = logging.getLogger(__file__)
+hdlr = logging.FileHandler(__file__ + ".log")
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+log.addHandler(hdlr)
+log.setLevel(logging.DEBUG)
 
 # U+250x 	─ 	━ 	│ 	┃ 	┄ 	┅ 	┆ 	┇ 	┈ 	┉ 	┊ 	┋ 	┌ 	┍ 	┎ 	┏
 # U+251x 	┐ 	┑ 	┒ 	┓ 	└ 	┕ 	┖ 	┗ 	┘ 	┙ 	┚ 	┛ 	├ 	┝ 	┞ 	┟
@@ -37,23 +45,23 @@ from enum import Enum
 FULL_CHAR_DICT = {
    # ltrb
     '0000': ' ',
-    '1010': '─',  '2020': '━',  '0000': '│',  '0000': '┃',
-    '0011': '┌',  '0021': '┍',  '0000': '┎',  '0022': '┏',
-    '1001': '┐',  '2001': '┑',  '0000': '┒',  '2002': '┓',  '0110': '└',  '0000': '┕',  '0000': '┖',  '0220': '┗',
-    '1100': '┘',  '2100': '┙',  '0000': '┚',  '2200': '┛',  '0000': '├',  '0000': '┝',  '0000': '┞',  '0000': '┟',
-    '0212': '┠',  '0221': '┡',  '0000': '┢',  '0000': '┣',  '0000': '┤',  '0000': '┥',  '0000': '┦',  '0000': '┧',
-    '1202': '┨',  '2201': '┩',  '0000': '┪',  '0000': '┫',  '1011': '┬',  '0000': '┭',  '0000': '┮',  '0000': '┯',
-    '1012': '┰',  '2012': '┱',  '0000': '┲',  '0000': '┳',  '1110': '┴',  '0000': '┵',  '0000': '┶',  '0000': '┷',
-    '1210': '┸',  '2210': '┹',  '0000': '┺',  '0000': '┻',  '0000': '┼',  '0000': '┽',  '0000': '┾',  '0000': '┿',
-    '1211': '╀',  '1112': '╁',  '0000': '╂',  '0000': '╃',  '0000': '╄',  '0000': '╅',  '0000': '╆',  '0000': '╇',
-    '2122': '╈',  '2212': '╉',  '0000': '╊',  '0000': '╋',  '0000': '╌',  '0000': '╍',  '0000': '╎',  '0000': '╏',
-    '3030': '═',  '0303': '║',  '0000': '╒',  '0000': '╓',  '0033': '╔',  '0000': '╕',  '0000': '╖',  '3003': '╗',
-    '0130': '╘',  '0310': '╙',  '0330': '╚',  '0000': '╛',  '0000': '╜',  '3300': '╝',  '0000': '╞',  '0000': '╟',
-    '0333': '╠',  '3101': '╡',  '0000': '╢',  '0000': '╣',  '0000': '╤',  '0000': '╥',  '0000': '╦',  '0000': '╧',
-    '1310': '╨',  '3330': '╩',  '0000': '╪',  '0000': '╫',  '0000': '╬',  
-    '1000': '╴',  '0100': '╵',  '0000': '╶',  '0000': '╷',  
-    '2000': '╸',  '0200': '╹',  '0000': '╺',  '0000': '╻',  
-    '1020': '╼',  '0102': '╽',  '0000': '╾',  '0000': '╿',  
+    '1010': '─',  '2020': '━',  '0101': '│',  '0000': '┃',
+    '0011': '┌',  '0021': '┍',  '0012': '┎',  '0022': '┏',
+    '1001': '┐',  '2001': '┑',  '1002': '┒',  '2002': '┓',  '0110': '└',  '0120': '┕',  '0210': '┖',  '0220': '┗',
+    '1100': '┘',  '2100': '┙',  '1200': '┚',  '2200': '┛',  '0111': '├',  '0121': '┝',  '0211': '┞',  '0112': '┟',
+    '0212': '┠',  '0221': '┡',  '0122': '┢',  '0222': '┣',  '1101': '┤',  '2101': '┥',  '1201': '┦',  '1102': '┧',
+    '1202': '┨',  '2201': '┩',  '2102': '┪',  '2202': '┫',  '1011': '┬',  '2011': '┭',  '1021': '┮',  '2021': '┯',
+    '1012': '┰',  '2012': '┱',  '1022': '┲',  '2022': '┳',  '1110': '┴',  '2110': '┵',  '1120': '┶',  '2120': '┷',
+    '1210': '┸',  '2210': '┹',  '1220': '┺',  '2220': '┻',  '1111': '┼',  '2111': '┽',  '1121': '┾',  '2121': '┿',
+    '1211': '╀',  '1112': '╁',  '1212': '╂',  '2211': '╃',  '1221': '╄',  '2112': '╅',  '1122': '╆',  '2221': '╇',
+    '2122': '╈',  '2212': '╉',  '1222': '╊',  '2222': '╋',
+    '3030': '═',  '0303': '║',  '0031': '╒',  '0013': '╓',  '0033': '╔',  '3001': '╕',  '1003': '╖',  '3003': '╗',
+    '0130': '╘',  '0310': '╙',  '0330': '╚',  '3100': '╛',  '1300': '╜',  '3300': '╝',  '0131': '╞',  '0313': '╟',
+    '0333': '╠',  '3101': '╡',  '1303': '╢',  '3303': '╣',  '3031': '╤',  '1013': '╥',  '3033': '╦',  '3130': '╧',
+    '1310': '╨',  '3330': '╩',  '3131': '╪',  '1313': '╫',  '3333': '╬',  
+    '1000': '╴',  '0100': '╵',  '0010': '╶',  '0001': '╷',  
+    '2000': '╸',  '0200': '╹',  '0020': '╺',  '0002': '╻',  
+    '1020': '╼',  '0102': '╽',  '2010': '╾',  '0201': '╿',  
 }
 
 FULL_CHAR_DICT['0000'] = ' '
@@ -263,7 +271,19 @@ class GridRenderer:
         #return "\n".join(grid_rows)
         return [''.join(row) for row in grid_rows]
 
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
 
+    def __str__(self):
+        return "Vec[{}, {}]".format(self.x, self.y)
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __mod__(self, other):
+        return Vector(self.x % other.x, self.y % other.y)
 
 
 class Crossword(npyscreen.widget.Widget):
@@ -276,43 +296,92 @@ class Crossword(npyscreen.widget.Widget):
         self.puzzle_words = cfg['words']
         self.puzzle_solution_col = cfg['solution_column']
 
-
         super(Crossword, self).__init__(screen, **kwargs)
+
+        self.cursor = Vector(self.puzzle_words[0][1], 0) # initialize cursor on first character of first word
 
         self.grid = GridRenderer(self.puzzle_width, self.puzzle_height)
 
         for n, (word, offset) in enumerate(self.puzzle_words):
             for i in range(len(word)):
                 self.grid.set_cell_edge(offset+i, n, LineType.THICK)
+                if i > 0:
+                    self.grid.set_cell_edge_left(offset+i, n, LineType.THIN_DASHED3)
+
+        #for i in range(len(self.puzzle_words)):
+            #self.grid.set_cell_edge_left (self.puzzle_solution_col, i, LineType.DOUBLE)
+            #self.grid.set_cell_edge_right(self.puzzle_solution_col, i, LineType.DOUBLE)
+
+
+        # user input is stored here
+        self.puzzle_input = [ [' '] * len(word) for word, _ in self.puzzle_words ]
 
         self.update()
+        log.info("created crossworld")
 
     def calculate_area_needed(self):
         return self.puzzle_height*2+1, self.puzzle_width*4+1
 
     def set_up_handlers(self):
         super(Crossword, self).set_up_handlers()
-        """
-        self.handlers.update({
+        self.handlers = { # overwrite existing handlers!
             curses.KEY_UP:    self.cursor_up,
             curses.KEY_DOWN:  self.cursor_down,
             curses.KEY_LEFT:  self.cursor_left,
             curses.KEY_RIGHT: self.cursor_right
-        })
-        """
+        }
 
-    def cursor_up(self):
-        pass
+        self.complex_handlers.append([
+            lambda _input: chr(_input).isalnum(),
+            self.handle_generic_input
+        ])
 
-    def cursor_down(self):
-        pass
+    def cursor_is_in_field(self):
+        if self.cursor.x < 0 or self.cursor.y < 0 or self.cursor.x >= self.puzzle_width or self.cursor.y >= self.puzzle_height:
+            return False
 
-    def cursor_left(self):
-        pass
+        word, offset = self.puzzle_words[self.cursor.y]
+        return offset <= self.cursor.x < offset+len(word)
 
-    def cursor_right(self):
-        pass
+    def cursor_move(self, x, y):
+        assert x != 0 or y != 0
 
+        while True:
+            self.cursor = (self.cursor + Vector(x, y)) % Vector(self.puzzle_width, self.puzzle_height)
+            if self.cursor_is_in_field():
+                break
+
+
+    def cursor_up(self, _input):
+        self.cursor_move(0, -1)
+
+    def cursor_down(self, _input):
+        self.cursor_move(0, 1)
+
+    def cursor_left(self, _input):
+        self.cursor_move(-1, 0)
+
+    def cursor_right(self, _input):
+        self.cursor_move(1, 0)
+
+    def cursor_home(self, _input):
+        pass # TODO
+
+    def cursor_end(self, _input):
+        pass # TODO
+
+    def handle_generic_input(self, _input):
+        cur_word, cur_offset = self.puzzle_words[self.cursor.y]
+        self.puzzle_input[self.cursor.y][self.cursor.x - cur_offset] = chr(_input).upper()
+
+        # move cursor to next character (or word if this was the last character)
+        if self.cursor.x-cur_offset >= len(cur_word) - 1:
+            # move to next word
+            self.cursor.y = (self.cursor.y + 1) % self.puzzle_height
+            self.cursor.x = self.puzzle_words[self.cursor.y][1]
+        else:
+            # move to next character
+            self.cursor.x += 1
 
     def update(self, clear=True):
         if clear:
@@ -325,18 +394,19 @@ class Crossword(npyscreen.widget.Widget):
         for n, line in enumerate(self.grid.render(3,1)):
             self.add_line(self.rely + n, self.relx, line, self.make_attributes_list(line, curses.A_NORMAL), self.calculate_area_needed()[1])
 
-        #for n, (word, offset) in enumerate(self.puzzle_words):
-            #self.add_line(self.rely + n, self.relx + offset, word, self.make_attributes_list(word, curses.A_NORMAL), self.puzzle_width)
-        """
-        for row in range(self.puzzle_height):
-            for col in range(self.puzzle_width):
-                self.parent.curses_pad.addch(self.rely + row*2, self.relx + col*4 + 1, "╚")
-                self.parent.curses_pad.addch(self.rely + row*2, self.relx + col*4 + 2, "+")
-                self.parent.curses_pad.addch(self.rely + row*2, self.relx + col*4 + 3, curses.ACS_ULCORNER)
-                #self.add_line(self.rely + row*3 + 2, self.relx + col*4 + 2, "+", 
-                        #[curses.A_NORMAL],
-                        #1)
-        """
+        # draw user input
+        for n, ((word, offset), user_input) in enumerate(zip(self.puzzle_words, self.puzzle_input)):
+            for i, char in enumerate(user_input):
+                attr = curses.A_NORMAL
+                if self.cursor.x == (i+offset) and self.cursor.y == n:
+                    # draw cursor
+                    attr = curses.A_STANDOUT | curses.A_BLINK
+
+                self.add_line(
+                        self.rely + n*2 + 1,
+                        self.relx + (i+offset)*4 + 2,
+                        char,
+                        [attr], 1)
 
 
 class CrosswordForm(npyscreen.Form):
@@ -356,7 +426,10 @@ class MyApplication(npyscreen.NPSAppManaged):
        # A real application might define more forms here.......
 
 if __name__ == '__main__':
+
+    log.info("starting application")
     with open('puzzle.cfg', 'r') as puzzle_cfg:
         cfg=json.load(puzzle_cfg)
-        print("got words:", cfg['words'])
+        log.info("using configuration: {}".format(cfg))
+
     TestApp = MyApplication().run()
