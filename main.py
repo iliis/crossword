4d9f18cb39254f3ebcf73ccd52b90c3746c03a11
harryhoudini <samuelbryner@gmx.ch>
2018-04-22 14:38:29 +0200

    Ignore CTRL+C (aka SIGINT)

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!/bin/env python3
 import curses
+import signal
 
 from app import Application, log
 
+# ignore CTRL+V keybinding
+signal.signal(signal.SIGINT, signal.SIG_IGN)
 
 def main(screen):
     app = Application(screen)
